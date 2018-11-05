d545cdf94aa72a63a37476335ff578ea3d59e186
黄信程 <huangxincheng@xmles.cn>
2018-11-05 14:45:33 +0800

    使用Feign进行调用

---
+++ microservicecloud-consumer-feign-dept/src/main/java/com/limaila/cloud/controller/DeptConsumerFeignController.java
@@ -0,0 +1,35 @@
+package com.limaila.cloud.controller;
+
+import com.limaila.cloud.entitys.Dept;
+import com.limaila.cloud.feign.DeptClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DeptConsumerFeignController {
+
+    @Autowired
+    private DeptClientService service;
+
+    @RequestMapping(value = "/consumer/dept/get/{id}")
+    public Dept get(@PathVariable("id") Long id)
+    {
+        return this.service.get(id);
+    }
+
+    @RequestMapping(value = "/consumer/dept/list")
+    public List<Dept> list()
+    {
+        return this.service.list();
+    }
+
+    @RequestMapping(value = "/consumer/dept/add")
+    public Object add(Dept dept)
+    {
+        return this.service.add(dept);
+    }
+}
