cef673937c1a7d90db3caef2c9e3cc80a6119db7
hxc <249270087@qq.com>
2018-11-11 20:05:10 +0800

    增加http代理相关测试

--- microservicecloud-provider-user/src/main/java/com/limaila/cloud/UserApplication.java
+++ microservicecloud-provider-user/src/main/java/com/limaila/cloud/UserApplication.java
@@ -4,12 +4,18 @@ import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
 import com.alicp.jetcache.anno.config.EnableMethodCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -29,6 +35,42 @@ import java.util.concurrent.ThreadPoolExecutor;
 @EnableAsync
 @EnableScheduling
 public class UserApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory){
+        RestTemplate restTemplate = new RestTemplate(factory);
+//        restTemplate.getMessageConverters().add(new WxMappingJackson2HttpMessageConverter());
+        /**
+         * restTemplate作为spring web client下的一个工具类 对http请求做了一层封装，用起来也更加简洁容易，
+         * 但最近遇到一个问题就是在发送请求时由于请求中包含中文导致乱码，都变成???????一堆问号，网上很多解决方案，但很多都比较…..
+         * 可以看到StringHttpMessageConverter默认是ISO-8859-1编码（有空可以看下别的MessageConverter其实都是UTF-8的），所以就把它改了就好啦~
+         * 先看说如何解决：
+         * restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+         */
+        restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        return restTemplate;
+    }
+
+    /**
+     * 配置ClientHttpRequestFactory
+     * @return
+     */
+    @Bean
+    public ClientHttpRequestFactory factory(){
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(10000);//单位为ms
+        factory.setConnectTimeout(10000);//单位为ms
+        return factory;
+    }
+
+
+
 
     /** Set the ThreadPoolExecutor's core pool size. */
     private int corePoolSize = 10;
@@ -63,7 +105,5 @@ public class UserApplication {
     }
 
 
-    public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
-    }
+
 }
--- microservicecloud-provider-user/src/test/java/com/limaila/cloud/UserApplicationTest.java
+++ microservicecloud-provider-user/src/test/java/com/limaila/cloud/UserApplicationTest.java
@@ -2,14 +2,17 @@ package com.limaila.cloud;
 
 import com.limaila.cloud.aysnc.AsyncTask;
 import com.limaila.cloud.entitys.User;
+import com.limaila.cloud.proxy.ProxyCustomizer;
 import com.limaila.cloud.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
 import java.util.concurrent.ExecutionException;
@@ -29,6 +32,12 @@ public class UserApplicationTest {
     @Autowired
     private AsyncTask asyncTask;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
     @Test
     public void contextLoads() throws ExecutionException, InterruptedException {
         System.out.println("----------------OK");
@@ -41,5 +50,14 @@ public class UserApplicationTest {
             }
         }
         System.out.println("----------------BB");
+    }
+
+    @Test
+    public void testHttpProxy() {
+        String str1 = restTemplate.getForObject("http://www.baidu.com", String.class);
+        String str2 = restTemplateBuilder.additionalCustomizers(new ProxyCustomizer()).build()
+                .getForObject("http://www.baidu.com", String.class);
+        System.out.println("===================================" + str1);
+        System.out.println("===================================" + str2);
     }
 }
