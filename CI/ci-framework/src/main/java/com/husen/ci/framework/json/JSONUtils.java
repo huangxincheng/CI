22f7ec4d5490a964adacadf51539363d65ab4c9a
huangxincheng <huangxincheng@xmiles.cn>
2019-06-09 16:52:58 +0800

    增加JSONutils

---
+++ CI/ci-framework/src/main/java/com/husen/ci/framework/json/JSONUtils.java
@@ -0,0 +1,42 @@
+package com.husen.ci.framework.json;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/***
+ @Author:MrHuang
+ @Date: 2019/6/9 16:38
+ @DESC: TODO
+ @VERSION: 1.0
+ ***/
+public class JSONUtils {
+
+    public static <T> T json2Bean(String jsonData, Class<T> clazz) {
+        return JSON.parseObject(jsonData, clazz);
+    }
+
+    public static<T> List<T> json2List(String jsonData, Class<T> clazz) {
+        return JSON.parseArray(jsonData, clazz);
+    }
+
+    public static Map<String, Object> json2Map(String jsonData) {
+        return JSON.parseObject(jsonData, TreeMap.class);
+    }
+
+    public static JSONObject json2JSONObject(String jsonData) {
+        return JSON.parseObject(jsonData);
+    }
+
+    public static JSONArray json2JSONArray(String jsondata) {
+        return JSON.parseArray(jsondata);
+    }
+
+    public String object2Json(Object object) {
+        return JSON.toJSONString(object);
+    }
+}
