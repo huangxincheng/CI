ea4a705a4b03b7c3148f822067bc72c3e104e1f0
huangxincheng <huangxincheng@xmiles.cn>
2019-06-20 17:33:03 +0800

    修改代码

--- CI/ci-framework/src/main/java/com/husen/ci/framework/net/HttpUtils.java
+++ CI/ci-framework/src/main/java/com/husen/ci/framework/net/HttpUtils.java
@@ -359,29 +359,33 @@ public class HttpUtils {
     }
 
     public static void main(String[] args) {
-        HttpResult result = HttpUtils.getInstance().doGet("http://www.baidu.com");
-        System.out.println(result);
-        String body = "{\n" +
-                "\t\"userId\": 10001,\n" +
-                "\t\"orderTitle\": \"order title\",\n" +
-                "\t\"orderRemake\": \"order remake\",\n" +
-                "\t\"orderAmount\": 1000\n" +
-                "}";
-        HttpResult result1 = HttpUtils.getInstance().doPostBody("http://www.limaila.com:30081/api/v1/user/saveOrder", body, null);
-        System.out.println(result);
+//        HttpResult result = HttpUtils.getInstance().doGet("http://www.baidu.com");
+//        System.out.println(result);
+//        String body = "{\n" +
+//                "\t\"userId\": 10001,\n" +
+//                "\t\"orderTitle\": \"order title\",\n" +
+//                "\t\"orderRemake\": \"order remake\",\n" +
+//                "\t\"orderAmount\": 1000\n" +
+//                "}";
+//        HttpResult result1 = HttpUtils.getInstance().doPostBody("http://www.limaila.com:30081/api/v1/user/saveOrder", body, null);
+//        System.out.println(result);
+//
+//        HttpUtils.getInstance()
+//                .doPostBodyWithAsync("http://www.limaila.com:30081/api/v1/user/saveOrder", body, null, new Callback() {
+//                    @Override
+//                    public void onFailure(Call call, IOException e) {
+//                        System.out.println("onFailure call = " + call.toString() + "e =" + e);
+//                    }
+//
+//                    @Override
+//                    public void onResponse(Call call, Response response) throws IOException {
+//                        System.out.println("onResponse call = " + call.toString() + "response =" + response);
+//                    }
+//                });
 
-        HttpUtils.getInstance()
-                .doPostBodyWithAsync("http://www.limaila.com:30081/api/v1/user/saveOrder", body, null, new Callback() {
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        System.out.println("onFailure call = " + call.toString() + "e =" + e);
-                    }
-
-                    @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-                        System.out.println("onResponse call = " + call.toString() + "response =" + response);
-                    }
-                });
+        String esUrl = "http://node1.es.sizne.net:9200/";
+        HttpResult result = HttpUtils.getInstance().doGet(esUrl);
+        System.out.println(result);
 
     }
 }
